a41787f22a9e07b2475e3a2057ed0def99f0a21e
Molly Badger <mrb9856@ad.rit.edu>
2020-02-20 17:13:58 -0500

    Created the classes and an enum for player states

---
+++ game/Schwartz's Sneaky Snail Mail Scandal/Schwartz's Sneaky Snail Mail Scandal/Erika.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
+{
+    class Erika
+    {
+    }
+}
---
+++ game/Schwartz's Sneaky Snail Mail Scandal/Schwartz's Sneaky Snail Mail Scandal/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
+{
+    enum States
+    {
+        FaceUp,
+        FaceDown,
+        FaceRight,
+        FaceLeft,
+        WalkUp,
+        WalkDown,
+        WalkLeft,
+        WalkRight
+    }
+    class Player
+    {
+
+    }
+}
