adbbbf0725c302fd2b90d74aa5c457e0a10aaa31
seb3418@g.rit.edu <seb3418@g.rit.edu>
2020-02-25 23:52:51 -0500

    Added Sprite Sheet, implemented walk cycle. Walking down works great, other directions do not work, need to find where state machine is broken. Will move to personal laptop as lab is closing.

--- game/Schwartz's Sneaky Snail Mail Scandal/Schwartz's Sneaky Snail Mail Scandal/Game1.cs
+++ game/Schwartz's Sneaky Snail Mail Scandal/Schwartz's Sneaky Snail Mail Scandal/Game1.cs
@@ -12,6 +12,9 @@ namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        //Player to draw based on state
+        Player player;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -41,7 +44,11 @@ namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            Vector2 playerLoc = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+
             Texture2D spriteSheet = Content.Load<Texture2D>("Ritchie");
+
+            player = new Player(spriteSheet, playerLoc, PlayerStates.FaceDown);
         }
 
         /// <summary>
@@ -65,7 +72,243 @@ namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
 
             // TODO: Add your update logic here
 
-            base.Update(gameTime);
+            //Handles animation
+            player.UpdateAnimation(gameTime);
+
+            //Get User input
+            KeyboardState kbState = Keyboard.GetState();
+
+            //Switch statement for Walking states
+            switch (player.State)
+            {
+                //Case for facing Down
+                case PlayerStates.FaceDown: 
+                    if(kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    if(kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    if(kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    if(kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.WalkDown;
+                    }
+                    //Sends back to still image
+                    if(kbState.IsKeyUp(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    break;
+
+                //Case for facing right
+                case PlayerStates.FaceRight:
+                    if (kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    if (kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    if (kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    if (kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.WalkRight;
+                    }
+                    //Sends back to still image
+                    if (kbState.IsKeyUp(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    break;
+
+                //Case for facing Left
+                case PlayerStates.FaceLeft:
+                    if (kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    if (kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    if (kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    if (kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.WalkLeft;
+                    }
+                    //Sends back to still image
+                    if (kbState.IsKeyUp(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    break;
+
+                //Case for Facing Up
+                case PlayerStates.FaceUp:
+                    if (kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    if (kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    if (kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    if (kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.WalkUp;
+                    }
+                    //Sends back to still image
+                    if (kbState.IsKeyUp(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    break;
+
+
+                //Case for walking Down
+                case PlayerStates.WalkDown:
+                    if (kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    if (kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    if (kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    if (kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.WalkDown;
+                    }
+                    //Sends back to still image
+                    if (kbState.IsKeyUp(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    break;
+
+                //Case for walking right
+                case PlayerStates.WalkRight:
+                    if (kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    if (kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    if (kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    if (kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.WalkRight;
+                    }
+                    //Sends back to still image
+                    if (kbState.IsKeyUp(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    break;
+
+                //Case for walking left
+                case PlayerStates.WalkLeft:
+                    if (kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    if (kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    if (kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    if (kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.WalkLeft;
+                    }
+                    //Sends back to still image
+                    if (kbState.IsKeyUp(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    break;
+
+                //Case for walking up
+                case PlayerStates.WalkUp:
+                    if (kbState.IsKeyDown(Keys.A) == true)
+                    {
+                        player.State = PlayerStates.FaceLeft;
+                    }
+                    if (kbState.IsKeyDown(Keys.D) == true)
+                    {
+                        player.State = PlayerStates.FaceRight;
+                    }
+                    if (kbState.IsKeyDown(Keys.S) == true)
+                    {
+                        player.State = PlayerStates.FaceDown;
+                    }
+                    if (kbState.IsKeyDown(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.WalkUp;
+                    }
+                    //Sends back to still image
+                    if (kbState.IsKeyUp(Keys.W) == true)
+                    {
+                        player.State = PlayerStates.FaceUp;
+                    }
+                    break;
+
+            }
+
+
+            //If statement to check for movement of walking
+            //Positive Y integer for walking down
+            if (player.State == PlayerStates.WalkDown)
+            {
+                player.Y = player.Y + 1; 
+            }
+            //Negative Y integer for walking up
+            if(player.State == PlayerStates.WalkUp)
+            {
+                player.Y = player.Y - 1;
+            }
+            //Positive X integer for walking right
+            if(player.State == PlayerStates.WalkRight)
+            {
+                player.X = player.X + 1;
+            }
+            //Negative X integer for walking left
+            if(player.State == PlayerStates.WalkLeft)
+            {
+                player.X = player.X - 1;
+            }
+
+
+                base.Update(gameTime);
         }
 
         /// <summary>
@@ -74,9 +317,15 @@ namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            player.Draw(spriteBatch);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- game/Schwartz's Sneaky Snail Mail Scandal/Schwartz's Sneaky Snail Mail Scandal/Player.cs
+++ game/Schwartz's Sneaky Snail Mail Scandal/Schwartz's Sneaky Snail Mail Scandal/Player.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
 {
-    enum States
+    enum PlayerStates
     {
         FaceUp,
         FaceDown,
@@ -27,7 +27,7 @@ namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
 
         Vector2 playerLoc;  // tracks player's location
         Texture2D spriteSheet;
-        States playerState;
+        PlayerStates state;
 
         // Constants for the sprites
 
@@ -50,102 +50,245 @@ namespace Schwartz_s_Sneaky_Snail_Mail_Scandal
             get { return this.playerLoc.X; }
             set { this.playerLoc.X = value; }
         }
-        public States PlayerState
+
+        public float Y
         {
-            get { return playerState; }
-            set { playerState = value; }
+            get { return this.playerLoc.Y; }
+            set { this.playerLoc.Y = value; }
         }
 
-        public Player(Texture2D spriteSheet, Vector2 playerLoc /*PlayerState startingState*/)
+        public PlayerStates State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public Player(Texture2D spriteSheet, Vector2 playerLoc, PlayerStates startingState)
         {
             this.spriteSheet = spriteSheet;
             this.playerLoc = playerLoc;
-            //this.playerState = playerState; to be added when PlayerState is working properly
+            this.state = startingState;
 
-            fps = 10.0;
+            fps = 0.0;
             timePerFrame = 1.0 / fps;
         }
 
-        public void Update(GameTime gameTime)
+        public void UpdateAnimation(GameTime gameTime)
         {
-            // FSM for player animation goes here
-            switch (playerState)
+            //Handle the animation timing and cycling
+            //Adds to the time counter to check if enough time ha passed
+
+            //Time passing
+            timeCounter += gameTime.ElapsedGameTime.TotalSeconds;
+
+            //If enough time has passed for each cycle
+            if(timeCounter >= timePerFrame)
             {
-                case (States.FaceDown):
-                    {
-                        break;
-                    }
-                case (States.FaceLeft):
-                    {
-                        break;
-                    }
-                case (States.FaceRight):
-                    {
-                        break;
-                    }
-                case (States.FaceUp):
-                    {
-                        break;
-                    }
-                case (States.WalkDown):
-                    {
-                        break;
-                    }
-                case (States.WalkLeft):
-                    {
-                        break;
-                    }
-                case (States.WalkRight):
-                    {
-                        break;
-                    }
-                case (States.WalkUp):
-                    {
-                        break;
-                    }
+                frame += 1;                     //Increments the frame images
+
+                if (frame > WalkFrameCount)     //Checking the bounds of the wlak cycle
+                    frame = 1;                  //Back to 1 for the frames
+
+                timeCounter -= timePerFrame;    //removes time used, helps keep time passing
             }
+
         }
 
-        public void Draw(GameTime gameTime)
+        //Method for drawing of each state
+        public void Draw(SpriteBatch spriteBatch)
         {
             // FSM for player animation goes here
-            switch (playerState)
+            switch (state)
             {
-                case (States.FaceDown):
+                case (PlayerStates.FaceDown):
                     {
+                        DrawFaceDown(SpriteEffects.None, spriteBatch);
                         break;
                     }
-                case (States.FaceLeft):
+                case (PlayerStates.FaceLeft):
                     {
+                        DrawFaceLeft(SpriteEffects.None, spriteBatch);
                         break;
                     }
-                case (States.FaceRight):
+                case (PlayerStates.FaceRight):
                     {
+                        DrawFaceRight(SpriteEffects.None, spriteBatch);
                         break;
                     }
-                case (States.FaceUp):
+                case (PlayerStates.FaceUp):
                     {
+                        DrawFaceUp(SpriteEffects.None, spriteBatch);
                         break;
                     }
-                case (States.WalkDown):
+                case (PlayerStates.WalkDown):
                     {
+                        DrawWalkingDown(SpriteEffects.None, spriteBatch);
                         break;
                     }
-                case (States.WalkLeft):
+                case (PlayerStates.WalkLeft):
                     {
+                        DrawWalkingLeft(SpriteEffects.None, spriteBatch);
                         break;
                     }
-                case (States.WalkRight):
+                case (PlayerStates.WalkRight):
                     {
+                        DrawWalkingRight(SpriteEffects.None, spriteBatch);
                         break;
                     }
-                case (States.WalkUp):
+                case (PlayerStates.WalkUp):
                     {
+                        DrawWalkingUp(SpriteEffects.None, spriteBatch);
                         break;
                     }
             }
 
 
+        }
+
+
+        //Methods for drawing the standing frames
+        private void DrawFaceDown(SpriteEffects flipsprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                spriteSheet,                    // - Texture to draw
+                playerLoc,                      // - Location to draw to
+                new Rectangle(                  // - Source Rectangle
+                    0,                          //   - Specifies where in the
+                    PlayerRectOffsetY,          //      sprite image to pull
+                    PlayerRectWidth,            //      the drawing from
+                    PlayerRectHeight),          //
+                Color.White,                    // - Color
+                0,                              // - Rotation (Should be none)
+                Vector2.Zero,                   // - Origin inside the image (top left of image)
+                1.0f,                           // - Scale (100% no change right now)
+                flipsprite,                     // - Used to flip image if needed
+                0);                             // - Layer depth will implement later
+        }
+
+        private void DrawFaceRight(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                spriteSheet,                    // - Texture to draw
+                playerLoc,                      // - Location to draw to
+                new Rectangle(                  // - Source Rectangle
+                    0,                          //   - Specifies where in the
+                    PlayerRectOffsetY,          //      sprite image to pull
+                    PlayerRectWidth,            //      the drawing from
+                    2 * PlayerRectHeight),          //
+                Color.White,                    // - Color
+                0,                              // - Rotation (Should be none)
+                Vector2.Zero,                   // - Origin inside the image (top left of image)
+                1.0f,                           // - Scale (100% no change right now)
+                flipSprite,                     // - Used to flip image if needed
+                0);                             // - Layer depth will implement later
+        }
+
+        private void DrawFaceLeft(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+               spriteSheet,                    // - Texture to draw
+               playerLoc,                      // - Location to draw to
+               new Rectangle(                  // - Source Rectangle
+                   0,                          //   - Specifies where in the
+                   PlayerRectOffsetY,          //      sprite image to pull
+                   PlayerRectWidth,            //      the drawing from
+                   3 * PlayerRectHeight),          //
+               Color.White,                    // - Color
+               0,                              // - Rotation (Should be none)
+               Vector2.Zero,                   // - Origin inside the image (top left of image)
+               1.0f,                           // - Scale (100% no change right now)
+               flipSprite,                     // - Used to flip image if needed
+               0);                             // - Layer depth will implement later
+        }
+
+        private void DrawFaceUp(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+               spriteSheet,                    // - Texture to draw
+               playerLoc,                      // - Location to draw to
+               new Rectangle(                  // - Source Rectangle
+                   0,                          //   - Specifies where in the
+                   PlayerRectOffsetY,          //      sprite image to pull
+                   PlayerRectWidth,            //      the drawing from
+                   4 * PlayerRectHeight),          //
+               Color.White,                    // - Color
+               0,                              // - Rotation (Should be none)
+               Vector2.Zero,                   // - Origin inside the image (top left of image)
+               1.0f,                           // - Scale (100% no change right now)
+               flipSprite,                     // - Used to flip image if needed
+               0);                             // - Layer depth will implement later
+        }
+
+        private void DrawWalkingDown(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+               spriteSheet,                    // - Texture to draw
+               playerLoc,                      // - Location to draw to
+               new Rectangle(                  // - Source Rectangle
+                   frame * PlayerRectWidth,    //   - Specifies where in the
+                   PlayerRectOffsetY,          //      sprite image to pull
+                   PlayerRectWidth,            //      the drawing from
+                   PlayerRectHeight),          //
+               Color.White,                    // - Color
+               0,                              // - Rotation (Should be none)
+               Vector2.Zero,                   // - Origin inside the image (top left of image)
+               1.0f,                           // - Scale (100% no change right now)
+               flipSprite,                     // - Used to flip image if needed
+               0);                             // - Layer depth will implement later
+        }
+
+        private void DrawWalkingRight(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+               spriteSheet,                    // - Texture to draw
+               playerLoc,                      // - Location to draw to
+               new Rectangle(                  // - Source Rectangle
+                   frame * PlayerRectWidth,    //   - Specifies where in the
+                   PlayerRectOffsetY,          //      sprite image to pull
+                   PlayerRectWidth,            //      the drawing from
+                   2 * PlayerRectHeight),          //
+               Color.White,                    // - Color
+               0,                              // - Rotation (Should be none)
+               Vector2.Zero,                   // - Origin inside the image (top left of image)
+               1.0f,                           // - Scale (100% no change right now)
+               flipSprite,                     // - Used to flip image if needed
+               0);                             // - Layer depth will implement later
+        }
+
+        private void DrawWalkingLeft(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+               spriteSheet,                    // - Texture to draw
+               playerLoc,                      // - Location to draw to
+               new Rectangle(                  // - Source Rectangle
+                   frame * PlayerRectWidth,    //   - Specifies where in the
+                   PlayerRectOffsetY,          //      sprite image to pull
+                   PlayerRectWidth,            //      the drawing from
+                   3 * PlayerRectHeight),          //
+               Color.White,                    // - Color
+               0,                              // - Rotation (Should be none)
+               Vector2.Zero,                   // - Origin inside the image (top left of image)
+               1.0f,                           // - Scale (100% no change right now)
+               flipSprite,                     // - Used to flip image if needed
+               0);                             // - Layer depth will implement later
+        }
+
+        private void DrawWalkingUp(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+               spriteSheet,                    // - Texture to draw
+               playerLoc,                      // - Location to draw to
+               new Rectangle(                  // - Source Rectangle
+                   frame * PlayerRectWidth,    //   - Specifies where in the
+                   PlayerRectOffsetY,          //      sprite image to pull
+                   PlayerRectWidth,            //      the drawing from
+                   4 * PlayerRectHeight),          //
+               Color.White,                    // - Color
+               0,                              // - Rotation (Should be none)
+               Vector2.Zero,                   // - Origin inside the image (top left of image)
+               1.0f,                           // - Scale (100% no change right now)
+               flipSprite,                     // - Used to flip image if needed
+               0);                             // - Layer depth will implement later
         }
     }
 }
