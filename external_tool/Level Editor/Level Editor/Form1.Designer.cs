2a1e50bd34cf08f2c80f68805b1025b0fa68a87e
juuu-jiii <juuu-jiii@users.noreply.github.com>
2020-03-31 16:59:49 +0800

    got the base idea for the level editor winform working. work still needed, as per to-do at top of Form1

---
+++ external_tool/Level Editor/Level Editor/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows.Forms;
+
+namespace Level_Editor
+{
+	partial class levelEditorForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tileSelectionComboBox = new System.Windows.Forms.ComboBox();
+			this.saveLayoutButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// tileSelectionComboBox
+			// 
+			this.tileSelectionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.tileSelectionComboBox.FormattingEnabled = true;
+			this.tileSelectionComboBox.Items.AddRange(new object[] {
+            "wall",
+            "floor"});
+			this.tileSelectionComboBox.Location = new System.Drawing.Point(54, 388);
+			this.tileSelectionComboBox.Name = "tileSelectionComboBox";
+			this.tileSelectionComboBox.Size = new System.Drawing.Size(121, 21);
+			this.tileSelectionComboBox.TabIndex = 3;
+			// 
+			// saveLayoutButton
+			// 
+			this.saveLayoutButton.Location = new System.Drawing.Point(54, 415);
+			this.saveLayoutButton.Name = "saveLayoutButton";
+			this.saveLayoutButton.Size = new System.Drawing.Size(75, 23);
+			this.saveLayoutButton.TabIndex = 4;
+			this.saveLayoutButton.Text = "Save Layout";
+			this.saveLayoutButton.UseVisualStyleBackColor = true;
+			this.saveLayoutButton.Click += new System.EventHandler(this.SaveLayoutButton_Click);
+			// 
+			// levelEditorForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.saveLayoutButton);
+			this.Controls.Add(this.tileSelectionComboBox);
+			this.Name = "levelEditorForm";
+			this.Text = "Level Editor";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			// 
+			// newPictureBox
+			// 
+			
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ComboBox tileSelectionComboBox;
+		private System.Windows.Forms.Button saveLayoutButton;
+	}
+}
+
---
+++ external_tool/Level Editor/Level Editor/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// TO-DO:
+// Ensure that all squares are filled before saving
+// Load from preset template
+// Get rid of all the magic numbers (use enums for writer chars, maybe use for colours too)
+
+namespace Level_Editor
+{
+	public partial class levelEditorForm : Form
+	{
+		StreamWriter writer;
+
+		// List to store all PictureBox objects used to represent map tiles.
+		List<PictureBox> tileList;
+
+		Color wallColor = Color.Gray;
+		Color floorColor = Color.Blue;
+
+		public levelEditorForm()
+		{
+			InitializeComponent();
+			tileList = new List<PictureBox>();
+		}
+			
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// Drawing a grid of PictureBox objects
+			// i = rows, j = columns
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					PictureBox newPictureBox = new PictureBox();
+
+					// Set PictureBox's parameters
+					newPictureBox.Name = "tile" + i + ":" + j;
+					newPictureBox.Width = 50;
+					newPictureBox.Height = 50;
+					newPictureBox.BackColor = Color.White;
+					// Offset PictureBoxes from each other by 10 pixels
+					// Note how type Point works; an instance must be created 
+					//		before assignation
+					newPictureBox.Location = new Point(
+						j * (2 + newPictureBox.Width) + 10,
+						i * (2 + newPictureBox.Height) + 10);
+
+					tileList.Add(newPictureBox);
+
+					// Important! Without this, controls will not display!
+					Controls.Add(newPictureBox);
+
+					// Adding click event handlers to each PictureBox manually
+					// Different var names must be used for object and EventArgs,
+					//		because this is nested in a method that already uses
+					//		object sender and EventArgs e. Any alt name will do.
+					void newPictureBox_Click(object newSender, EventArgs newE)
+					{
+						// Change colour of PictureBoxes when they are clicked on
+						//		depending on the selected ComboBox value
+						switch (tileSelectionComboBox.Text)
+						{
+							case ("wall"):
+								{
+									newPictureBox.BackColor = Color.Gray;
+									break;
+								}
+							case ("floor"):
+								{
+									newPictureBox.BackColor = Color.Blue;
+									break;
+								}
+							default:
+								{
+									Console.WriteLine("This shouldn't happen");
+									break;
+								}
+						}
+					}
+
+					// Manually subscribing the form to these click events
+					// This must be done because the PictureBoxes only exist 
+					//		AFTER the form is created - WYSIWYG was not used.
+					newPictureBox.Click += new EventHandler(newPictureBox_Click);
+				}
+			}
+		}
+
+		private void SaveToFile(string filename)
+		{
+			try
+			{
+				writer = new StreamWriter(filename);
+
+				int lineBreakCtrl = 0;
+
+				// Looping through tileList and taking each PictureBox's 
+				//		BackColor to guide save file writing
+				foreach (PictureBox tile in tileList)
+				{
+					// Create a new line every 5 chars
+					if (lineBreakCtrl == 5)
+					{
+						writer.WriteLine();
+						lineBreakCtrl = 0;
+					}
+
+					if (tile.BackColor == wallColor)
+					{
+						// Separate using spaces for now
+						writer.Write("W ");
+					}
+					else if (tile.BackColor == floorColor)
+					{
+						// Separate using spaces for now
+						writer.Write("F ");
+					}
+					
+					lineBreakCtrl++;
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				if (writer != null)
+				{
+					writer.Close();
+				}
+			}
+		}
+
+		private void SaveLayoutButton_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog fileDialog = new SaveFileDialog();
+			fileDialog.ShowDialog();
+			string filename = fileDialog.FileName;
+			this.SaveToFile(filename);
+		}
+	}
+}
