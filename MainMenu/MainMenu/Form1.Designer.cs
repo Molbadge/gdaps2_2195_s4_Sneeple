2fa8dfa089fbed9f3ce0a31b07d44267ccb1e090
Molly Badger <mrb9856@ad.rit.edu>
2020-03-31 11:16:55 -0400

    Created MainMenu Form in solutions

---
+++ MainMenu/MainMenu/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace MainMenu
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.Play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Argon Tight", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Title.Location = new System.Drawing.Point(50, 27);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(870, 49);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Schwartz\'s Sneaky Snail Mail Scandal";
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.Black;
+            this.Play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Play.Font = new System.Drawing.Font("Argon Tight", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Play.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Play.Location = new System.Drawing.Point(273, 289);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(400, 94);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(976, 577);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Title);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button Play;
+    }
+}
+
