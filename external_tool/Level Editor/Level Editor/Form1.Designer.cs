7c1cfbd832735be4da8373c2310bb68daee21929
juuu-jiii <juuu-jiii@users.noreply.github.com>
2020-04-03 18:42:10 +0800

    Cleaned up magic numbers. Program now prevents user from saving changes if any square is left blank. Load functionality added. Save algorithm modified to work with load functionality. Added label on top that displays the current file being worked on. Added label at bottom that displays messages and warnings to the user. Added comments to newly-implemented code. Pretty much done with this for now lol bye

--- external_tool/Level Editor/Level Editor/Form1.Designer.cs
+++ external_tool/Level Editor/Level Editor/Form1.Designer.cs
@@ -32,6 +32,9 @@ namespace Level_Editor
 		{
 			this.tileSelectionComboBox = new System.Windows.Forms.ComboBox();
 			this.saveLayoutButton = new System.Windows.Forms.Button();
+			this.messageLabel = new System.Windows.Forms.Label();
+			this.loadLayoutButton = new System.Windows.Forms.Button();
+			this.currentMapLabel = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// tileSelectionComboBox
@@ -56,27 +59,58 @@ namespace Level_Editor
 			this.saveLayoutButton.UseVisualStyleBackColor = true;
 			this.saveLayoutButton.Click += new System.EventHandler(this.SaveLayoutButton_Click);
 			// 
+			// messageLabel
+			// 
+			this.messageLabel.AutoSize = true;
+			this.messageLabel.Location = new System.Drawing.Point(51, 367);
+			this.messageLabel.Name = "messageLabel";
+			this.messageLabel.Size = new System.Drawing.Size(323, 13);
+			this.messageLabel.TabIndex = 5;
+			this.messageLabel.Text = "Select a tile type from the dropdown. Click on the grid to place tiles.";
+			// 
+			// loadLayoutButton
+			// 
+			this.loadLayoutButton.Location = new System.Drawing.Point(135, 415);
+			this.loadLayoutButton.Name = "loadLayoutButton";
+			this.loadLayoutButton.Size = new System.Drawing.Size(75, 23);
+			this.loadLayoutButton.TabIndex = 6;
+			this.loadLayoutButton.Text = "Load Layout";
+			this.loadLayoutButton.UseVisualStyleBackColor = true;
+			this.loadLayoutButton.Click += new System.EventHandler(this.LoadLayoutButton_Click);
+			// 
+			// currentMapLabel
+			// 
+			this.currentMapLabel.AutoSize = true;
+			this.currentMapLabel.Location = new System.Drawing.Point(51, 9);
+			this.currentMapLabel.Name = "currentMapLabel";
+			this.currentMapLabel.Size = new System.Drawing.Size(155, 13);
+			this.currentMapLabel.TabIndex = 7;
+			this.currentMapLabel.Text = "Currently working on: New Map";
+			// 
 			// levelEditorForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.currentMapLabel);
+			this.Controls.Add(this.loadLayoutButton);
+			this.Controls.Add(this.messageLabel);
 			this.Controls.Add(this.saveLayoutButton);
 			this.Controls.Add(this.tileSelectionComboBox);
 			this.Name = "levelEditorForm";
 			this.Text = "Level Editor";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
-			// 
-			// newPictureBox
-			// 
-			
+			this.PerformLayout();
 
 		}
 
 		#endregion
 		private System.Windows.Forms.ComboBox tileSelectionComboBox;
 		private System.Windows.Forms.Button saveLayoutButton;
+		private Label messageLabel;
+		private Button loadLayoutButton;
+		private Label currentMapLabel;
 	}
 }
 
--- external_tool/Level Editor/Level Editor/Form1.cs
+++ external_tool/Level Editor/Level Editor/Form1.cs
@@ -7,52 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-
-// TO-DO:
-// Ensure that all squares are filled before saving
-// Load from preset template
-// Get rid of all the magic numbers (use enums for writer chars, maybe use for colours too)
+using System.IO; // Needed for file IO
 
 namespace Level_Editor
 {
 	public partial class levelEditorForm : Form
 	{
+		// File IO variables
+		FileStream readStream;
 		StreamWriter writer;
+		StreamReader reader;
 
 		// List to store all PictureBox objects used to represent map tiles.
 		List<PictureBox> tileList;
 
+		// Variables to replace magic numbers
 		Color wallColor = Color.Gray;
 		Color floorColor = Color.Blue;
+		Color emptySquareColor = Color.White;
+		int mapLength = 5;
+		int mapHeight = 5;
 
 		public levelEditorForm()
 		{
 			InitializeComponent();
 			tileList = new List<PictureBox>();
 		}
-			
+
+		// Loading and creating all controls that were not created via WYSIWYG
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			// Drawing a grid of PictureBox objects
-			// i = rows, j = columns
-			for (int i = 0; i < 5; i++)
+			for (int rows = 0; rows < mapHeight; rows++)
 			{
-				for (int j = 0; j < 5; j++)
+				for (int columns = 0; columns < mapLength; columns++)
 				{
 					PictureBox newPictureBox = new PictureBox();
 
 					// Set PictureBox's parameters
-					newPictureBox.Name = "tile" + i + ":" + j;
 					newPictureBox.Width = 50;
 					newPictureBox.Height = 50;
-					newPictureBox.BackColor = Color.White;
+					newPictureBox.BackColor = emptySquareColor;
 					// Offset PictureBoxes from each other by 10 pixels
 					// Note how type Point works; an instance must be created 
 					//		before assignation
 					newPictureBox.Location = new Point(
-						j * (2 + newPictureBox.Width) + 10,
-						i * (2 + newPictureBox.Height) + 10);
+						columns * (2 + newPictureBox.Width) + 51,
+						rows * (2 + newPictureBox.Height) + 30);
 
 					tileList.Add(newPictureBox);
 
@@ -75,17 +76,25 @@ namespace Level_Editor
 						{
 							case ("wall"):
 								{
-									newPictureBox.BackColor = Color.Gray;
+									newPictureBox.BackColor = wallColor;
+									messageLabel.ForeColor = Color.Black;
+									messageLabel.Text =
+										"Select a tile type from the dropdown. Click on the grid to place tiles.";
 									break;
 								}
 							case ("floor"):
 								{
-									newPictureBox.BackColor = Color.Blue;
+									newPictureBox.BackColor = floorColor;
+									messageLabel.ForeColor = Color.Black;
+									messageLabel.Text =
+										"Select a tile type from the dropdown. Click on the grid to place tiles.";
 									break;
 								}
 							default:
 								{
-									Console.WriteLine("This shouldn't happen");
+									messageLabel.ForeColor = Color.Black;
+									messageLabel.Text =
+										"Select a tile type from the dropdown first.";
 									break;
 								}
 						}
@@ -99,42 +108,84 @@ namespace Level_Editor
 			}
 		}
 
+		/// <summary>
+		/// Helper method to check if all tiles on the map grid are filled.
+		/// </summary>
+		/// <returns>
+		/// Returns true if all tiles on the map grid are filled, and false
+		///		otherwise.
+		/// </returns>
+		private bool CheckAllTilesFilled()
+		{
+			foreach (PictureBox tile in tileList)
+			{
+				if (tile.BackColor == emptySquareColor)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Writes all changes made on the map grid to a save file.
+		/// </summary>
+		/// <param name="filename">
+		/// The location to write save data to.
+		/// </param>
 		private void SaveToFile(string filename)
 		{
 			try
 			{
 				writer = new StreamWriter(filename);
 
+				// Variable track the number of characters written in a line.
 				int lineBreakCtrl = 0;
 
 				// Looping through tileList and taking each PictureBox's 
 				//		BackColor to guide save file writing
 				foreach (PictureBox tile in tileList)
 				{
-					// Create a new line every 5 chars
-					if (lineBreakCtrl == 5)
+					if (tile.BackColor == wallColor)
+					{
+						// Separate chars using spaces for now
+						writer.Write("W");
+					}
+					else if (tile.BackColor == floorColor)
+					{
+						// Separate chars using spaces for now
+						writer.Write("F");
+					}
+
+					lineBreakCtrl++;
+
+					// Create a new line every time map length limit is reached.
+					if (lineBreakCtrl == mapLength)
 					{
 						writer.WriteLine();
 						lineBreakCtrl = 0;
 					}
-
-					if (tile.BackColor == wallColor)
+					// Otherwise, separate characters with a space character.
+					else
 					{
-						// Separate using spaces for now
-						writer.Write("W ");
-					}
-					else if (tile.BackColor == floorColor)
-					{
-						// Separate using spaces for now
-						writer.Write("F ");
-					}
-					
-					lineBreakCtrl++;
+						writer.Write(" ");
+					}	
 				}
+
+				// Alter currentMapLabel to display the filepath of the map currently 
+				//		being worked on.
+				currentMapLabel.Text = "Currently working on: " + filename;
+
+				// Make messageLabel display a confirmation message.
+				messageLabel.ForeColor = Color.Green;
+				messageLabel.Text = "Save successful - Saved file to: " + filename;
 			}
-			catch (Exception e)
+			catch
 			{
-				Console.WriteLine(e.Message);
+				// Make messageLabel display an error message.
+				messageLabel.ForeColor = Color.Red;
+				messageLabel.Text = "Error encountered while attempting to save data.";
 			}
 			finally
 			{
@@ -145,12 +196,123 @@ namespace Level_Editor
 			}
 		}
 
+		/// <summary>
+		/// Helper method to interpret save file data.
+		/// </summary>
+		/// <param name="letter">
+		/// Each individual letter from the save file.
+		/// </param>
+		/// <returns>
+		/// Returns one of three colours depending on the letter supplied from 
+		///		the save file.
+		/// </returns>
+		private Color AnalyzeColor(string letter)
+		{
+			switch (letter)
+			{
+				case ("F"):
+					{
+						return floorColor;
+					}
+				case ("W"):
+					{
+						return wallColor;
+					}
+					// Any other character results in the default square colour 
+					//		and an error message.
+				default:
+					{
+						messageLabel.ForeColor = Color.Red;
+						messageLabel.Text = "Error - save file data invalid or corrupted.";
+						return emptySquareColor;
+					}
+			}
+		}
+
+		/// <summary>
+		/// Reads map grid data from a save file and displays the result on 
+		///		the screen.
+		/// </summary>
+		/// <param name="filename">
+		/// The location of the save file to read from.
+		/// </param>
+		private void LoadFromFile(string filename)
+		{
+			try
+			{
+				readStream = File.OpenRead(filename);
+				reader = new StreamReader(readStream);
+				string lineOfText = null;
+
+				// List to keep a running inventory of all results of the 
+				//		split.
+				List<string> tileTypeList = new List<string>();
+
+				while ((lineOfText = reader.ReadLine()) != null)
+				{
+					// Array to hold the results of the currently split line.
+					string[] splitArray = lineOfText.Split(' ');
+
+					for (int i = 0; i < splitArray.Length; i++)
+					{
+						// Adding to the running inventory in the list.
+						tileTypeList.Add(splitArray[i]);
+					}
+				}
+
+				// Loop through the list of PictureBoxes, and applying the 
+				//		appropriate BackColor.
+				// A foreach loop was unsuitable, since indices were important.
+				for (int i = 0; i < tileList.Count; i++)
+				{
+					tileList[i].BackColor = AnalyzeColor(tileTypeList[i]);
+				}
+
+				// Alter currentMapLabel to display the filepath of the map currently 
+				//		being worked on.
+				currentMapLabel.Text = "Currently working on: " + filename;
+
+				// Make messageLabel display a confirmation message.
+				messageLabel.ForeColor = Color.Green;
+				messageLabel.Text = "Load successful - now working on: " + filename;
+			}
+			catch
+			{
+				// Make messageLabel display an error message.
+				messageLabel.ForeColor = Color.Red;
+				messageLabel.Text = "Error - save file data incompatible or corrupted.";
+			}
+			finally
+			{
+				if (reader != null)
+				{
+					reader.Close();
+				}
+			}
+		}
+
 		private void SaveLayoutButton_Click(object sender, EventArgs e)
 		{
+			// Do not allow user to save work if tiles are not all filled.
+			if (!CheckAllTilesFilled())
+			{
+				messageLabel.ForeColor = Color.Red;
+				messageLabel.Text = "All squares must be filled!";
+				return;
+			}
+
 			SaveFileDialog fileDialog = new SaveFileDialog();
 			fileDialog.ShowDialog();
 			string filename = fileDialog.FileName;
 			this.SaveToFile(filename);
+		}
+
+		private void LoadLayoutButton_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog fileDialog = new OpenFileDialog();
+			fileDialog.ShowDialog();
+			string filename = fileDialog.FileName;
+			this.LoadFromFile(filename);
 		}
 	}
 }
